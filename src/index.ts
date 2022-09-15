4091981f1a5eb36d712d2e18099dfe6d59f7feac
Linus Björklund <weedzcokie@gmail.com>
2022-09-15 15:12:52 +0200

    Add `mergeUpdateStore()`
    This allows you to update part of an object without destructuring.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 export type PartialStoreListener<T, K extends keyof T> = (arg: T[K]) => void;
 
+type DeepPartial<T> = Partial<{ [P in keyof T]: DeepPartial<T[P]> }>;
+
 export interface CreatedStore<StoreType extends Record<string, unknown>, StoreKeys extends keyof StoreType> {
     Store: Readonly<StoreType>
     subscribe: <K extends StoreKeys>(key: K, cb: PartialStoreListener<StoreType, K>) => () => void
     updateStore: (newStore: Partial<StoreType>) => void
+    mergeUpdateStore: (partialStoreItem: DeepPartial<StoreType>) => void
+}
+
+function deepMerge(source: Record<string, unknown> | {}, target: Record<string, unknown>) {
+    for (const [key, value] of Object.entries(source)) {
+        if (value !== null && typeof value === "object") {
+            if (target[key] === undefined) {
+                target[key] = value;
+            } else {
+                deepMerge(value, target[key] as Record<string, unknown>)
+            }
+        } else {
+            target[key] = value;
+        }
+    }
+    return target;
 }
 
 export function createStore<StoreType extends Record<string, unknown>>(initialState: StoreType): CreatedStore<StoreType, keyof StoreType> {
@@ -27,7 +45,7 @@ export function createStore<StoreType extends Record<string, unknown>>(initialSt
         listeners[key].splice(listeners[key].indexOf(cb as PartialStoreListener<StoreType, StoreKeys>)>>>0, 1);
     }
 
-    function triggerKeyListeners(newStore: Partial<StoreType>) {
+    function triggerKeyListeners(newStore: DeepPartial<StoreType>) {
         for (const key of Object.keys(newStore) as StoreKeys[]) {
             for (const listener of listeners[key]) {
                 const data = newStore[key] as StoreType[typeof key];
@@ -45,6 +63,10 @@ export function createStore<StoreType extends Record<string, unknown>>(initialSt
         updateStore(newStore: Partial<StoreType>) {
             triggerKeyListeners(newStore);
             Object.assign(store, newStore);
-        }
+        },
+        mergeUpdateStore(partialStoreItem: DeepPartial<StoreType>) {
+            triggerKeyListeners(partialStoreItem);
+            deepMerge(partialStoreItem, store);
+        },
     }
 }
