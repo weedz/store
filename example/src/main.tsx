75a5d6b1df120e43880b5a6c70fd905f8062fbcf
Linus Björklund <weedzcokie@gmail.com>
2024-06-29 18:27:53 +0200

    chore: Update dependencies

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -3,16 +3,16 @@ import { useStore } from "./store";
 import { userLogin, userLogout } from "./user";
 
 function Navbar() {
-    const {data} = useStore(["user"]);
+    const { data } = useStore(["user"]);
     const items = data.user
         ? [
-                <li>Hello, {data.user.name}</li>,
-                <li><button onClick={userLogout} type="button">Logout</button></li>,
+            <li key="greet">Hello, {data.user.name}</li>,
+            <li key="logout"><button onClick={userLogout} type="button">Logout</button></li>,
         ]
         : [
-                <li><button onClick={userLogin} type="button">Login</button></li>,
+            <li key="login"><button onClick={userLogin} type="button">Login</button></li>,
         ];
-    
+
     return (
         <ul>{items}</ul>
     )
